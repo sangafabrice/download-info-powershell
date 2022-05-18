8aef8122c944cb530df1b5092513f140f4dcb05b
sangafabrice <69244030+sangafabrice@users.noreply.github.com>
2022-05-18 05:34:34 +0200

    remove 4th argument

--- GetFrom-Sourceforge.bat
+++ GetFrom-Sourceforge.bat
@@ -15,7 +15,7 @@
 : Optional parser: it does not work when URLs
 : have special characters like %20 that is
 : whitespace encoded
-If Not ""=="%~2%~3%~4" (
+If Not ""=="%~2%~3" (
     For /F "Tokens=1* Delims==" %%U In ('Call "%~f0" %1') Do (
         If "version"=="%%U" If Not ""=="%~2" ( Set "%~2=%%V" ) Else Echo %%U=%%V
         If "link"=="%%U" If Not ""=="%~3" ( Set "%~3=%%V" ) Else Echo %%U=%%V
