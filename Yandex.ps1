9f6d28a9958f214abc60151e6817656d37ec84d9
Fabrice Sanga <69244030+sangafabrice@users.noreply.github.com>
2022-09-12 09:52:08 +0200

    update yandex script

--- Yandex.ps1
+++ Yandex.ps1
@@ -4,21 +4,33 @@
 #>
 
 Try {
-    (
-        (Invoke-WebRequest 'https://browser.yandex.ru/corp' -Verbose:$False).Links |
-        Where-Object outerHTML -Like "*Windows $(Switch ($OSArch) { 'x64' { 'x64' } 'x86' { 'x32' } })*" |
-        Select-Object href -Unique
-    ).href |
-    Select-Object @{
-        Name = 'Version'
-        Expression = {
-            [version] (
-                (($_ -split '/')?[-3] -split '_',4 |
-                ForEach-Object { $i = 0 } { If ($i -eq 3) { ($_ -split '_')[0] } Else { $_ } $i++ }) -join '.'
-            )
-        }
-    },@{
-        Name = 'Link'
-        Expression = { $_ }
-    }
+    [pscustomobject] @{
+        Version = $(
+            ((Invoke-WebRequest 'https://browser.yandex.ru/corp' -Verbose:$False).Links |
+            Where-Object outerHTML -Like "*Windows $(Switch ($OSArch) { 'x64' { 'x64' } 'x86' { 'x32' } })*").outerHTML |
+            ForEach-Object { 
+                [void] ($_ -match '(?<Version>(\d+\.)+\d+)')
+                $Matches.Version
+            } | Select-Object -Unique -First 1
+        )
+        Resource = $(
+            @{
+                Uri = 'https://browser.yandex.ru/corp/api/download/get/'
+                Method = 'POST'
+                ContentType = 'text/plain;charset=UTF-8'
+                Body = ('{{"platform":"win_{0}"}}' -f $(Switch ($OSArch) { 'x64' { 'x64' } 'x86' { 'x32' } }))
+            } | ForEach-Object { [uri] ("$(Invoke-WebRequest @_ -Verbose:$False)" | ConvertFrom-Json).url } |
+            Select-Object @{
+                Name = 'Link'
+                Expression = { $_ }
+            },@{
+                Name = 'Name'
+                Expression = {
+                    ([uri] $_).query -replace '\?' -split '&' |
+                    Where-Object { $_ -like 'filename=*' } |
+                    ForEach-Object { ($_ -split '=')?[-1] }
+                }
+            }
+        )
+    } | Select-Object Version -ExpandProperty Resource
 } Catch { }
