393e06f7bbf2e9a87066d3ac14796d623cb0f70e
Fabrice Sanga <69244030+sangafabrice@users.noreply.github.com>
2022-09-20 08:21:19 +0200

    add prepros downloadinfo script

---
+++ Prepros.ps1
@@ -0,0 +1,22 @@
+<#
+    Configuration file --
+#>
+
+Try {
+    @{
+        Uri = 'https://prepros.io/downloads/stable/windows'
+        Method  = 'HEAD'
+        MaximumRedirection = 0
+        SkipHttpErrorCheck = $True
+        ErrorAction = 'SilentlyContinue'
+        Verbose = $False
+    } | ForEach-Object { (Invoke-WebRequest @_).Headers.Location } |
+    Select-Object @{
+        Name = 'Version'
+        Expression = { [version] (([uri] $_).Segments?[-2] -replace '/$') }
+    },@{
+        Name = 'Link'
+        Expression = { $_ }
+    }
+}
+Catch { }
